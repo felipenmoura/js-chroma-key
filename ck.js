0e37b6a366fd686724895b85b9763f77c2eb3b02
Felipe N. Moura <felipenmoura@gmail.com>
2014-10-03 00:41:49 -0300

    .

--- ck.js
+++ ck.js
@@ -8,11 +8,28 @@ window.ck= (function(_d){
         _width  = 0,
         _height = 0;
     
+    
+    // função executada para cada quadro
+    var _videoPlaying= function(){
+        
+        var frame= '',
+            data= null;
+        
+        // na primeira vez, pega as dimenções do video
+        if(!_width){
+            _width= _video.offsetWidth;
+            _height= _video.offsetHeight;
+        }
+        
+        _ctx.drawImage(_video, 0, 0, _width, _height);
+        //_ctx.drawImage(_video, -200, -200, _width + 400, _height + 400);
+        
+    };
+    //
+    
     var _constructor= function(){
         
-        // um objecto URL
         _w.URL= _w.URL || _w.webkitURL;
-        
         _n.getUserMedia= _n.getUserMedia||_n.webkitGetUserMedia||_n.mozGetUserMedia||false;
         
         if(_n.getUserMedia){
@@ -22,15 +39,16 @@ window.ck= (function(_d){
                 video: true
             }, function(stream){
                 
-                // transformamos o stream em um objeto URL
                 try{
                     stream= _w.URL.createObjectURL(stream);
                 }catch(e){}
                 
-                // e o passamos para o src do elemento de vídeo
                 _video.src= stream;
-                // agora, podemos dar play no vídeo
                 _video.play();
+                
+                // atualizamos o canvas a cada quadro
+                //requestAnimationFrame(_videoPlaying);
+                setInterval(_videoPlaying, 60);
                 
             }, function(){
                 alert('O usuário não permitiu!');
