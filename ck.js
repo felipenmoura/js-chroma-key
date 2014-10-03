844e0c719bb011ccb0bb38907a6eea31edc1a9b3
Felipe N. Moura <felipenmoura@gmail.com>
2014-10-03 00:27:47 -0300

    .

--- ck.js
+++ ck.js
@@ -9,7 +9,27 @@ window.ck= (function(_d){
         _height = 0;
     
     var _constructor= function(){
-        // código aqui
+        // verificamos qual acesso ao userMedia está disponível
+        _n.getUserMedia= _n.getUserMedia||_n.webkitGetUserMedia||_n.mozGetUserMedia||false;
+        
+        // se houver suporte...
+        if(_n.getUserMedia){
+            
+            // solicitamos o uso da mídia
+            _n.getUserMedia({
+                audio: false,
+                video: true
+            }, function(stream){
+                
+                // usaremos o stream aqui
+                
+            }, function(){
+                alert('O usuário não permitiu!');
+            });
+            
+        }else{
+            alert('Não suportado!');
+        }
     }
     
     return {
