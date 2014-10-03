53f7fd3a7b201229702310ca5d3998ab8a306983
Felipe N. Moura <felipenmoura@gmail.com>
2014-10-02 23:51:38 -0300

    .

--- bkp.js
+++ bkp.js
@@ -1,48 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chroma key using JavaScript</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        
-        <style type="text/css">
-            canvas{
-                display: block;
-                border: solid 1px red;
-                background-image: url(bg1.png);
-                background-size: 100% 100%;
-                margin: auto;
-            }
-            video{
-                border: solid 1px red;
-                position: absolute;
-                top: -999px;
-            }
-            body{
-                background-color: appworkspace;
-            }
-        </style>
-        
-    </head>
-    <body>
-        <div style="text-align: center;">
-            <input type="button" value="Start" onclick="window.ck.start();" /><br/>
-            <video id="video" autoplay="autoplay" width="360" height="270"></video>
-            <canvas id="canvas" width="360" height="270"></canvas><br/>
-            <input id="range" type="range" value="100" min="0" max="255" />
-        </div>
-        <div style='width: 400px; margin: auto; margin-top: 20px;'>
-            <em>- Click the Start buttono and allow the page to use your camera;</em><br/>
-            <em>- Click the image to capture the current pixel;</em><br/>
-            <em>- Use the slider to change the sensitivity;</em><br/>
-            <em>- See the code or fork it at <a href='https://github.com/felipenmoura/js-chroma-key/'>Github's Felipe N. Moura projects list</a>;</em><br/>
-        </div>
-        
-        <script>
-            
-            
-            window.ck= (function(_d){
+window.ck= (function(_d){
     
     var _video= _d.getElementById('video'),
         _canvas= _d.getElementById('canvas'),
@@ -145,8 +101,3 @@
     
 })(document);
             
-            
-        </script>
-        
-    </body>
-</html>
